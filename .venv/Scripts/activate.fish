efb5525f71a11d0d725d24905e71e93436ecb05e
Zech Schweitzer <zschw@bu.edu>
2026-09-02 14:09:53 -0400

    modified requirements file

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,11 +33,11 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'c:\Users\zschw\Documents\BU\Fall 2026\Repositories\zechnschweitzer.github.io\.venv-1'
+set -gx VIRTUAL_ENV 'C:\Users\zschw\Documents\BU\Fall 2026\Repositories\zechnschweitzer.github.io\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
-set -gx VIRTUAL_ENV_PROMPT .venv-1
+set -gx VIRTUAL_ENV_PROMPT .venv
 
 # Unset PYTHONHOME if set.
 if set -q PYTHONHOME
@@ -57,7 +57,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s(%s)%s " (set_color 4B8BBE) .venv-1 (set_color normal)
+        printf "%s(%s)%s " (set_color 4B8BBE) .venv (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
